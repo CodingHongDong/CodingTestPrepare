f2f2045316569037070a43d220361524f4750502
CodingHongDong <101644470+CodingHongDong@users.noreply.github.com>
2022-11-18 20:31:15 +0900

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,22 @@
+package codingtest;
+
+class Solution {
+    
+	public int solution(int price) {
+        int answer = 0;
+        
+        if(price >= 500000) {
+        	answer =  (int) ((int) price - (price * 0.05));
+        	
+        } else if(price >= 300000) {
+        	answer = (int) ((int) price - (price * 0.1));
+        } else if(price >= 100000) {
+        	answer = (int) ((int) price - (price * 0.2));
+        } else {
+        	answer = price;
+        }
+        
+        return answer;
+    }
+	
+}
